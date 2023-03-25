6ed5676d746e95eb76c5b889eb9189a48c2ae133
Andrei Kazachkov <an.kazachkov3234@gmail.com>
2023-03-25 08:59:43 +0300

    The specifier %s works

--- printf.asm
+++ printf.asm
@@ -2,7 +2,10 @@
 		global _start
 
 section .data
-		msg: db "Hello, Andrei!", 10, 0
+		msg: db "Hel %sander%s", 0
+		name: db  "Alex", 0
+		bob: db "and Bob", 0
+
 
 section .text
 
@@ -14,16 +17,9 @@ section .text
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 _start:
 		mov rdi, msg
-		mov al, "A"
-		call  __strchr		; rsi -> symbol
-		
-		mov rdi, rsi
-		call __strlen		; length A in rcx
-		mov rdx, rcx
-
-		mov rax, 1
-		mov rdi, rax
-		syscall
+		mov rsi, name
+		mov rdx, bob
+		call _printf
 
 		mov rax, 0x3C		;exit64(rdi)
 		xor rdi, rdi
@@ -33,6 +29,104 @@ _start:
 %endif
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+;==========================================================
+;printf
+;==========================================================
+;----------------------------------------------------------
+;
+;----------------------------------------------------------
+;
+;
+;==========================================================
+_printf:	
+		push rbp
+        mov rbp, rsp
+		
+		push r9
+		push r8
+		push rcx
+		push rdx
+		push rsi
+
+		xor rdx, rdx		; buffer
+		dec rdx
+		mov r8, rdi			; string
+		dec r8
+	
+.next:	inc r8
+		inc rdx				; distance between specifiers
+		cmp byte [r8], 0	; end of str
+		je .end
+		cmp byte [r8], 0x25	; 0x25 = %
+		jne .next
+		
+		inc r8
+		mov rcx, [r8]		; specifier
+		and rcx, 127		; mask = 0111 1111
+		sub rcx, 'b'
+
+		sub r8, rdx
+		dec r8
+		mov rsi, r8
+		mov rax, 1
+		mov rdi, rax
+		push rcx
+		syscall
+		pop rcx
+		add r8, rdx
+		inc r8
+
+		pop rsi
+		jmp [.jumpTable + rcx*8]
+
+
+.jumpTable:	dq .b			; binary mode
+		dq .c			; char
+		dq .d			; int
+		times 10 dq (.default)
+		dq .o			; octal
+		times 3 dq (.default)
+		dq .s			; string
+		times 4 dq (.default)
+		dq .x			; hex
+		times 2 dq (.default)
+
+
+.s:		mov rdi, rsi
+		call __strlen		; in rdx - length
+		mov rdi, 1
+		mov rax, rdi
+		syscall
+		xor rdx, rdx		; buffer
+		dec rdx
+		jmp .next
+		
+		
+		jmp .next
+
+.c:		jmp .next
+
+.d:		jmp .next
+
+.b:		jmp .next
+
+.o:		jmp .next
+
+.x:		jmp .next
+
+.default:	jmp .next			
+		
+
+.end:	mov rsi, r8
+		sub rsi, rdx
+		mov rax, 1
+		mov rdi, rax
+		syscall
+
+		mov rsp, rbp
+		pop rdi
+
+		ret
 
 ;==========================================================
 ;strchr
@@ -83,20 +177,20 @@ __strchr:	push rdi
 ;Arguments:
 ;	1) rdi (pointer to the string)
 ;Returns:
-;	1) rcx (string length)
+;	1) rdx (string length)
 ;Destroys:
 ;	- FLAGS
-;	- rcx
+;	- rdx
 ;==========================================================
 
 __strlen:	push rdi
 
-		xor rcx, rcx
+		xor rdx, rdx
 		mov al, 0
 
 .Next:		scasb
 		je .End
-		inc rcx
+		inc rdx
 		jmp .Next	
 	
 .End:		pop rdi
